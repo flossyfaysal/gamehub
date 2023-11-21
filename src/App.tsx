aab76e8f8433ad25b543565c7941fe70dffb8eb8
flossyfaysal <skfaysalalom@gmail.com>
2023-11-22 00:10:05 +0600

    Integrated : ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@chakra-ui/react'
 
 function App() {
- return <Button colorScheme='blue'>Button</Button>;
+ return <Button colorScheme='blue'>Click</Button>;
 }
 
 export default App
