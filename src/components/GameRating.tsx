53fb73844716970e54288d77e9978edd5ec21550
flossyfaysal <skfaysalalom@gmail.com>
2023-12-08 23:59:22 +0600

    emoji

---
+++ src/components/GameRating.tsx
@@ -0,0 +1,25 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+import { BsAlarm, BsApple, BsArrowDownLeft } from "react-icons/bs";
+
+interface Props{
+    rating: number;
+}
+
+function GameRating({rating}: Props) {
+
+    if( rating < 3 ) null;
+
+    const ratingMap: {[key:number] : ImageProps} = {
+        3: { src: "<BsAlarm/>", alt:'meh'},
+        4: { src: "<BsApple />", alt:'recommended'},
+        5: { src: "<BsArrowDownLeft/>", alt: 'great'}
+    }
+
+  return (
+    <div>
+        <Image {...ratingMap[rating]}/>
+    </div>
+  )
+}
+
+export default GameRating
