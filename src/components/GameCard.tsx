05504972a629259d3cc5c26eb336f66296cf58ba
flossyfaysal <skfaysalalom@gmail.com>
2023-12-09 21:33:27 +0600

    Fixed

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,31 +1,29 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
-import {Game} from "../hooks/useGames"
+import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
 import CriticScore from "./CriticScore"
 import croppedImageUrl from "../services/cropped-image"
-import GameRating from "./GameRating"
 
-interface Props{
+interface Props {
     game: Game
 }
 
-function GameCard({game}: Props) {
-  return(
-    <Card>
-        <Image src={ croppedImageUrl(game.background_image)} />
-        <CardBody>
-            <HStack justifyContent="space-between">
-                <PlatformIconList 
-                    platforms={ game.parent_platforms.map( p => p.platform )}
+function GameCard({ game }: Props) {
+    return (
+        <Card>
+            <Image src={croppedImageUrl(game.background_image)} />
+            <CardBody>
+                <HStack justifyContent="space-between">
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map(p => p.platform)}
                     />
-                <CriticScore score={game.metacritic}/>
+                    <CriticScore score={game.metacritic} />
 
-            </HStack>
-            <Heading>{game.name}/</Heading>
-            <GameRating rating={game.rating_top}/>
-        </CardBody>
-    </Card>
-  )
+                </HStack>
+                <Heading>{game.name}/</Heading>
+            </CardBody>
+        </Card>
+    )
 }
 
 export default GameCard
